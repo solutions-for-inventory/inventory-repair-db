47b32fc0f9f40de054da9a57545a79f4559364da
Jorge Anez <jorge.anez@coxautoinc.com>
2020-11-23 21:19:23 -0400

    jenkins fix

--- sql/V1__auth_dml.sql
+++ sql/V1__auth_dml.sql
@@ -39,3 +39,223 @@ CREATE TABLE authz_approval (
     expires_at timestamptz NOT NULL,
     PRIMARY KEY (uid, client_id)
 );
+
+create table t_org_unit (
+    org_unit_id   bigint default nextval('t_org_unit_org_unit_id_seq'::regclass) not null constraint t_org_unit_pkey primary key,
+    name          varchar                                                        not null,
+    org_code      varchar                                                        not null constraint unique_org_unit unique,
+    webpage       varchar                                                        not null,
+    facebook      varchar                                                        not null,
+    status        varchar                                                        not null,
+    locale        varchar                                                        not null,
+    created_date  timestamp with time zone                                       not null,
+    modified_date timestamp with time zone
+);
+
+create table t_role (
+    role_id       bigint default nextval('t_role_role_id_seq'::regclass)                not null constraint t_role_pkey primary key,
+    key           varchar                  not null,
+    name          varchar                  not null,
+    description   varchar,
+    active        boolean                  not null,
+    created_date  timestamp with time zone not null,
+    modified_date timestamp with time zone
+);
+
+create table t_privilege (
+    privilege_id  bigint default nextval('t_privilege_privilege_id_seq'::regclass) not null constraint t_privilege_pkey primary key,
+    key           varchar                                                          not null,
+    name          varchar                                                          not null,
+    description   varchar,
+    active        boolean                                                          not null,
+    created_date  timestamp with time zone                                         not null,
+    modified_date timestamp with time zone
+);
+
+create table t_role_privilege (
+    role_privilege_id bigint default nextval('t_role_privilege_role_privilege_id_seq'::regclass) not null constraint t_role_privilege_pkey primary key,
+    role_id           bigint                                                                     not null constraint t_role_privilege_role_id_fkey references t_role,
+    privilege_id      bigint                                                                     not null constraint t_role_privilege_privilege_id_fkey references t_privilege (privilege_id)
+);
+
+create table t_person (
+    person_id     bigint default nextval('t_person_person_id_seq'::regclass) not null constraint t_person_pkey primary key,
+    first_name    varchar                                                    not null,
+    last_name     varchar                                                    not null,
+    document_type varchar                                                    not null,
+    document_id   varchar                                                    not null constraint unique_person_document_id unique,
+    org_unit_id   bigint                                                     not null constraint t_person_org_unit_id_fkey references t_org_unit,
+    created_date  timestamp with time zone                                   not null,
+    modified_date timestamp with time zone
+);
+
+create table t_user (
+    user_id               bigint default nextval('t_user_user_id_seq'::regclass) not null constraint t_user_pkey primary key,
+    username              varchar                  not null constraint unique_user_username unique,
+    email                 varchar                  not null constraint unique_user_email unique,
+    password              varchar                  not null,
+    status                varchar                  not null,
+    locale                varchar                  not null,
+    expiration            boolean                  not null,
+    new_password_required boolean                  not null,
+    person_id             bigint                   not null constraint t_user_person_id_fkey references t_person,
+    created_date          timestamp with time zone not null,
+    modified_date         timestamp with time zone
+);
+
+create table t_org_user_scope (
+    org_user_scope_id bigint default nextval('t_org_user_scope_org_user_scope_id_seq'::regclass) not null constraint t_org_user_scope_pkey primary key,
+    user_id           bigint    not null constraint t_org_user_scope_user_id_fkey references t_user,
+    org_unit_id       bigint    not null constraint t_org_user_scope_org_unit_id_fkey references t_org_unit
+);
+
+create table t_user_role (
+    user_role_id bigint default nextval('t_user_role_user_role_id_seq'::regclass) not null constraint t_user_role_pkey primary key,
+    user_id      bigint    not null constraint t_user_role_user_id_fkey references t_user,
+    role_id      bigint    not null constraint t_user_role_role_id_fkey references t_role
+);
+
+create table t_address (
+    address_id    bigint default nextval('t_address_address_id_seq'::regclass) not null constraint t_address_pkey primary key,
+    street1       varchar                                                      not null,
+    street2       varchar                                                      not null,
+    street3       varchar                                                      not null,
+    zip           varchar                                                      not null,
+    city          varchar                                                      not null,
+    state         varchar                                                      not null,
+    country       varchar                                                      not null,
+    person_id     bigint not null constraint unique_address_person_id unique constraint t_address_person_id_fkey references t_person,
+    created_date  timestamp with time zone                                     not null,
+    modified_date timestamp with time zone
+);
+
+create table t_contact_info (
+    contact_info_id bigint default nextval('t_contact_info_contact_info_id_seq'::regclass) not null constraint t_contact_info_pkey primary key,
+    contact_type    varchar                                                                not null,
+    contact         varchar                                                                not null,
+    person_id       bigint                                                                 not null constraint t_contact_info_person_id_fkey references t_person,
+    created_date    timestamp with time zone                                               not null,
+    modified_date   timestamp with time zone
+);
+
+create table t_category (
+    category_id   bigint default nextval('t_category_category_id_seq'::regclass) not null constraint t_category_pkey primary key,
+    name          varchar                                                        not null,
+    key           varchar,
+    description   varchar                                                        not null,
+    scope         varchar                                                        not null,
+    org_unit_id   bigint                                                         not null constraint t_category_org_unit_id_fkey references t_org_unit,
+    created_date  timestamp with time zone                                       not null,
+    modified_date timestamp with time zone
+);
+
+create table t_inventory (
+    inventory_id          bigint default nextval('t_inventory_inventory_id_seq'::regclass) not null constraint t_inventory_pkey primary key,
+    name                  varchar                                                          not null constraint unique_inventory_name unique,
+    description           varchar                                                          not null,
+    status                varchar                                                          not null,
+    allow_negative_stocks boolean                                                          not null,
+    org_unit_id           bigint                                                           not null,
+    created_date          timestamp with time zone                                         not null,
+    modified_date         timestamp with time zone
+);
+
+create table t_unit (
+    unit_id       bigint default nextval('t_unit_unit_id_seq'::regclass) not null constraint t_unit_pkey primary key,
+    key           varchar                                                not null,
+    label         varchar                                                not null,
+    created_date  timestamp with time zone                               not null,
+    modified_date timestamp with time zone
+);
+
+create table t_item (
+    item_id       bigint default nextval('t_item_item_id_seq'::regclass) not null constraint t_item_pkey primary key,
+    code          varchar                                                not null,
+    name          varchar                                                not null,
+    default_price double precision                                       not null,
+    description   varchar,
+    images        varchar                                                not null,
+    part_number   varchar,
+    manufacturer  varchar,
+    model         varchar,
+    item_type     varchar                                                not null,
+    notes         varchar,
+    status        varchar                                                not null,
+    category_id   bigint constraint t_item_category_id_fkey references t_category,
+    unit_id       bigint constraint t_item_unit_id_fkey references t_unit,
+    org_unit_id   bigint                                                 not null constraint t_item_org_unit_id_fkey references t_org_unit,
+    created_date  timestamp with time zone                               not null,
+    modified_date timestamp with time zone
+);
+
+create table t_inventory_item (
+    inventory_item_id bigint default nextval('t_inventory_item_inventory_item_id_seq'::regclass) not null constraint t_inventory_item_pkey primary key,
+    status            varchar                                                                    not null,
+    level             bigint                                                                     not null,
+    max_level_allowed bigint                                                                     not null,
+    min_level_allowed bigint                                                                     not null,
+    price             double precision                                                           not null,
+    location          varchar                                                                    not null,
+    date_expiry       timestamp with time zone,
+    item_id           bigint                                                                     not null,
+    inventory_id      bigint                                                                     not null,
+    created_date      timestamp with time zone                                                   not null,
+    modified_date     timestamp with time zone
+);
+
+create table t_inventory_item_order (
+    inventory_item_order_id bigint default nextval('t_inventory_item_order_inventory_item_order_id_seq'::regclass) not null constraint t_inventory_item_order_pkey primary key,
+    quantity                bigint                                          not null,
+    price                   double precision                                not null,
+    discount                double precision                                not null,
+    sub_total_price         double precision                                not null,
+    notes                   varchar                                         not null,
+    inventory_item_id       bigint                                          not null,
+    oder_id                 bigint                                          not null,
+    created_date            timestamp with time zone                        not null,
+    modified_date           timestamp with time zone
+);
+
+create table t_order (
+    order_id            bigint default nextval('t_order_order_id_seq'::regclass) not null constraint t_order_pkey primary key,
+    discount            double precision                                         not null,
+    order_date          timestamp with time zone                                 not null,
+    delivered_lead_time timestamp with time zone                                 not null,
+    status              varchar                                                  not null,
+    notes               varchar                                                  not null,
+    inventory_id        bigint                                                   not null,
+    supplier_id         bigint                                                   not null,
+    emitter_id          bigint                                                   not null,
+    org_unit_id         bigint                                                   not null,
+    created_date        timestamp with time zone                                 not null,
+    modified_date       timestamp with time zone
+);
+
+create table t_supplier (
+    supplier_id   bigint default nextval('t_supplier_supplier_id_seq'::regclass) not null constraint t_supplier_pkey primary key,
+    name          varchar                                                        not null,
+    webpage       varchar                                                        not null,
+    contact_id    bigint                                                         not null,
+    org_unit_id   bigint                                                         not null,
+    created_date  timestamp with time zone                                       not null,
+    modified_date timestamp with time zone
+);
+
+create sequence t_org_unit_org_unit_id_seq;
+create sequence t_org_user_scope_org_user_scope_id_seq;
+create sequence t_user_user_id_seq;
+create sequence t_role_role_id_seq;
+create sequence t_privilege_privilege_id_seq;
+create sequence t_user_role_user_role_id_seq;
+create sequence t_role_privilege_role_privilege_id_seq;
+create sequence t_person_person_id_seq;
+create sequence t_address_address_id_seq;
+create sequence t_contact_info_contact_info_id_seq;
+create sequence t_category_category_id_seq;
+create sequence t_item_item_id_seq;
+create sequence t_inventory_inventory_id_seq;
+create sequence t_unit_unit_id_seq;
+create sequence t_inventory_item_inventory_item_id_seq;
+create sequence t_inventory_item_order_inventory_item_order_id_seq;
+create sequence t_order_order_id_seq;
+create sequence t_supplier_supplier_id_seq;
