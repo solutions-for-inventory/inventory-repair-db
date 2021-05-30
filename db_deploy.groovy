13c3071c460de1b2ed26abf5a9d98757aa25f09f
Jorge Anez <jorge.anez@coxautoinc.com>
2021-05-29 23:39:25 -0400

    db deploy

--- db_deploy.groovy
+++ db_deploy.groovy
@@ -3,25 +3,16 @@
 pipeline {
     agent any
     parameters {
-        choice(
-                name: 'env',
-                choices: ['--one--'], description: 'some input'
-        )
-        text(name: 'db_host', defaultValue: '192.168.0.100', description: 'Enter some information about the person')
-        text(name: 'db_user', defaultValue: 'inventory_user', description: 'db user')
-        password(name: 'db_password', defaultValueAsSecret: 'inventory_password', description: 'Enter a password')
+        string(name: 'DB_HOST', defaultValue: '192.168.0.100', description: 'db host')
+        string(name: 'DB_USER', defaultValue: 'inventory_user', description: 'db user')
+        string(name: 'DB_PASSWORD', defaultValue: 'inventory_password', description: 'db password')
     }
-
     stages {
-        stage('Run DB patch') {
+        stage('Example') {
             steps {
                 sh 'echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin'
                 sh 'docker pull ghcr.io/solutions-for-inventory/inventory-repair-db:1.0'
-            }
-
-            steps {
-                echo 'Deploying....'
-                sh "docker run --rm -v FLYWAY_URL:jdbc:postgresql://${params.db_host}:5432/inventory_repair_db -v FLYWAY_USER:${params.db_user} -v FLYWAY_PASSWORD:${params.db_password} ghcr.io/solutions-for-inventory/inventory-repair-db:1.0 migrate"
+                sh "docker run --rm -e FLYWAY_URL=jdbc:postgresql://${params.DB_HOST}:5432/inventory_repair_db -e FLYWAY_USER=${params.DB_USER} -e FLYWAY_PASSWORD=${params.DB_PASSWORD} ghcr.io/solutions-for-inventory/inventory-repair-db:1.0 migrate"
             }
         }
     }
